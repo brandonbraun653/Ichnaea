0bd207542146601cdb64d5060833013bfb6e1e36
Brandon Braun <brandonbraun653@protonmail.com>
2024-02-15 06:57:29 -0700

    Add virtual environment. Layout some schematic ideas. Add design doc for LTC7871.

---
+++ env_setup.sh
@@ -0,0 +1,8 @@
+poetry config virtualenvs.in-project true
+poetry config virtualenvs.path "$(pwd)"/.venv
+poetry config virtualenvs.create true
+poetry env use python3.11
+poetry install --no-interaction --no-root
+
+# Add packages with Pip that don't build correctly with Poetry
+poetry run pip install --upgrade pip
