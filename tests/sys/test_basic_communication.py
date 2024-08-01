0389773adc53ff0a45a3686e94dc0648c6dfd445
Brandon Braun <brandonbraun653@protonmail.com>
2024-08-01 13:57:20 -0700

    Fix serial driver issue with attempts to flood the rx buffers.

--- tests/sys/test_basic_communication.py
+++ tests/sys/test_basic_communication.py
@@ -19,3 +19,13 @@ class TestBasicRPCCommunication:
         for node in client.available_nodes:
             assert client.get_input_voltage(node) is not None, f"Node {node} did not return valid input voltage"
             assert client.get_output_voltage(node) is not None, f"Node {node} did not return valid output voltage"
+
+    def test_rx_overwhelm_with_requests(self, client):
+        """ Tests overwhelming the RX buffer with requests. """
+        assert len(client.available_nodes) > 0, "No nodes found in the system"
+
+        for node in client.available_nodes:
+            for x in range(100):
+                assert client.ping_node(node), f"Node {node} did not respond to ping {x}"
+            for x in range(100):
+                assert client.get_input_voltage(node) is not None, f"Node {node} did not return valid input voltage {x}"
